fdfb5eec3e91f82f751f30883cddcc00ac3e7573
JohannIngi <johannb16@ru.is>
2017-05-01 13:00:23 +0000

    First version of code

---
+++ goatCode/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(goatCode.Startup))]
+namespace goatCode
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
