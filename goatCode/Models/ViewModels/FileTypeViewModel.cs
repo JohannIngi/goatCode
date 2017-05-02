8dbffc3bd93443870b24f44700c695a903168035
manisigt <mani16@ru.is>
2017-05-02 20:41:47 +0000

    problem vid ad pulla

---
+++ goatCode/Models/ViewModels/FileTypeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace goatCode.Models.ViewModels
+{
+    public class FileTypeViewModel
+    {
+        public int Id { get; set; }
+    }
+}
