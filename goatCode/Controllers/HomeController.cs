557f0e873b813ea640f65addb41dad63442184c4
Arnar Pétursson <arnarp16@ru.is>
2017-05-05 23:46:24 +0000

    CodeCleanup: HomeController

--- goatCode/Controllers/HomeController.cs
+++ goatCode/Controllers/HomeController.cs
@@ -10,31 +10,11 @@ namespace goatCode.Controllers
     {
         public ActionResult Index()
         {
+            if(User.Identity.IsAuthenticated)
+            {
+                // Specific UserView
+            }
             return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Máni er ekki sætur gaur";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Feel free to contact us mjéeen.";
-
-            return View();
-        }
-        public ActionResult Editor()
-        {
-            ViewBag.Message = "I'm an editor, hello.";
-
-            return View();
-        }
-        public ActionResult viewProjects()
-        {
-            return RedirectToAction("UserProjects");
         }
     }
 }
--- goatCode/Models/IdentityModels.cs
+++ goatCode/Models/IdentityModels.cs
@@ -23,12 +23,7 @@ namespace goatCode.Models
     {
         public DbSet<File> Files { get; set; }
         public DbSet<Project> Projects { get; set; }
-#pragma warning disable CS0114 // Member hides inherited member; missing override keyword
-        public DbSet<AspNetUser> Users { get; set; }
-#pragma warning restore CS0114 // Member hides inherited member; missing override keyword
-
         public DbSet<FileType> FileTypes { get; set; }
-
         public DbSet<UserProject> UserProjects { get; set; }
 
 
