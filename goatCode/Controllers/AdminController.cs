3dcc4b9e3aff729f0f899e64c153999352178651
antona14 <antona14@ru.is>
2017-05-07 21:40:13 +0000

    bunch of comments

--- goatCode/Controllers/AdminController.cs
+++ goatCode/Controllers/AdminController.cs
@@ -10,46 +10,46 @@ namespace goatCode.Controllers
     /// <summary>
     /// 
     /// </summary>
-    public class AdminController : Controller
-    {
+    
         // GET: Admin
 
         /// <summary>
         /// Displays an index page.
         /// </summary>
         /// <returns>An index page</returns>
-    [Authorize(Roles = "Admin")]
-    public class AdminController : Controller
-    {
-        private FileService fservice = new FileService();
-        private ProjectService pservice = new ProjectService();
-        private UserService uservice = new UserService();
-        /// <summary>
-        /// Index 
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult Index()
+        [Authorize(Roles = "Admin")]
+        public class AdminController : Controller
         {
-            ViewBag.Files = fservice.GetAllFiles().Count;
-            ViewBag.Projects = pservice.GetAllProjects().Count();
-            ViewBag.Users = uservice.GetAllUsers().Count;
-            
-            return View();
+            private FileService fservice = new FileService();
+            private ProjectService pservice = new ProjectService();
+            private UserService uservice = new UserService();
+            /// <summary>
+            /// Index 
+            /// </summary>
+            /// <returns></returns>
+            public ActionResult Index()
+            {
+                ViewBag.Files = fservice.GetAllFiles().Count;
+                ViewBag.Projects = pservice.GetAllProjects().Count();
+                ViewBag.Users = uservice.GetAllUsers().Count;
+
+                return View();
+            }
+            public ActionResult Files()
+            {
+                var model = fservice.GetAllFiles();
+                return View(model);
+            }
+            public ActionResult Projects()
+            {
+                var model = pservice.GetAllProjects();
+                return View(model);
+            }
+            public ActionResult Users()
+            {
+                var model = uservice.GetAllUsers();
+                return View(model);
+            }
         }
-        public ActionResult Files()
-        {
-            var model = fservice.GetAllFiles();
-            return View(model);
-        }
-        public ActionResult Projects()
-        {
-            var model = pservice.GetAllProjects();
-            return View(model);
-        }
-        public ActionResult Users()
-        {
-            var model = uservice.GetAllUsers();
-            return View(model);
-        }
-    }
+    
 }
