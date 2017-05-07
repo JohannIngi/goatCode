4ade4fe5c16780d671e89f3edef93d09b3281dd0
Arnar Pétursson <arnarp16@ru.is>
2017-05-07 13:30:22 +0000

    HomeController redirects to admin index

--- goatCode/Controllers/AdminController.cs
+++ goatCode/Controllers/AdminController.cs
@@ -9,6 +9,7 @@ namespace goatCode.Controllers
     public class AdminController : Controller
     {
         // GET: Admin
+        [Authorize(Roles = "Admin")]
         public ActionResult Index()
         {
             return View();
