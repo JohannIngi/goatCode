5c2d2360d3ec5a95100d3a7929d30d7b6619f403
manisigt <mani16@ru.is>
2017-05-08 13:42:15 +0000

    signalR setup

---
+++ goatCode/Hubs/CodeHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace goatCode.Hubs
+{
+    public class CodeHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
