ade6bbb25b24b06b80cae57d7b49692fcf7f1d41
antona14 <antona14@ru.is>
2017-05-09 17:21:26 +0000

    ErrorPopups

--- goatCode/Controllers/UserController.cs
+++ goatCode/Controllers/UserController.cs
@@ -42,19 +42,19 @@ namespace goatCode.Controllers
             Project project = new Project();
             return View(project);
         }
-        public ActionResult User()
-        {
-            return RedirectToAction("");
-        }
         [HttpPost]
         [ValidateInput(false)]
         public ActionResult Create(Project project)
         {
-            project.name = HttpUtility.HtmlEncode(project.name);
+            if (ModelState.IsValid)
+            {
+                project.name = HttpUtility.HtmlEncode(project.name);
 
-            pservice.AddNewProject(project, User.Identity.GetUserId());           
+                pservice.AddNewProject(project, User.Identity.GetUserId());           
 
-            return RedirectToAction("Index");
+                return RedirectToAction("Index");
+            }
+            return View(project);
         }
 
         /// <summary>
@@ -83,11 +83,13 @@ namespace goatCode.Controllers
             {
                 if (uservice.IsUserOwner(uservice.GetUserIdByName(model.email), model.projectId) == true)
                 {
-                    // Ef user er owner  þá getur hann ekki share-að
+                    // Ef user er owner þá getur hann ekki share-að
+                    return View("UserIsOwnerError");
                 }
                 else if (uservice.IsUserRelatedToProject(uservice.GetUserIdByName(model.email), model.projectId) == true)
                 {
                     // Ef user er nú þegar tengdur við project þá gerist ekkert
+                    return View("CantShareWithThisUserError");
                 }
                 else
                 {
