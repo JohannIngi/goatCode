ba0565fc3f4fac46feacda419def78407d239b93
JohannIngi <johannb16@ru.is>
2017-05-12 10:56:10 +0000

    fixing log in view

--- goatCode/Controllers/AccountController.cs
+++ goatCode/Controllers/AccountController.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using goatCode.Models;
-using goatCode.Services;
 
 namespace goatCode.Controllers
 {
